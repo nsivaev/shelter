09d76ff158148dc6e689ba2b3f84830d651d7130
nsivaev <nsivaev@list.ru>
2023-04-04 19:46:12 +0300

    почти доделал бургер-меню

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('ЧИТАТЬ! score 60 + 40. подчеркивание для активных ссылок сделаю через js на этой неделе');
+const toggleBtn = document.getElementById('burger');
+const block = document.querySelector('.burger-menu-absolute');
+const links = block.querySelectorAll('a');
+
+
+toggleBtn.addEventListener('click', (event) => {
+    event.stopPropagation(); // Остановить всплытие события, чтобы не сработало обработчик события на document
+    block.classList.toggle('show'); // Переключаем класс для показа/скрытия блока
+
+    if (block.classList.contains('show')) {
+        document.body.style.overflow = 'hidden'; // Запретить прокручивание страницы
+    } else {
+        document.body.style.overflow = ''; // Разрешить прокручивание страницы
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if (!block.contains(event.target)) {
+        block.classList.remove('show'); // Скрываем блок, если было нажато вне его
+    }
+});
+
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        block.classList.remove('show'); // Скрываем блок при клике на ссылку внутри блока
+        document.body.style.overflow = ''; // Разрешаем прокручивание страницы после закрытия блока
+    });
+});
