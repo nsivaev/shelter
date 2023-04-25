da81dc24a91b07d1954ef7fd3a102312fc66d7ce
nsivaev <nsivaev@list.ru>
2023-04-25 22:45:30 +0300

    добавил рандом-слайдер на petsPage и исправил имена для png

--- scripts/petsPageSlider.js
+++ scripts/petsPageSlider.js
@@ -1,21 +1,22 @@
 //pagination
 const prevMaxBtn = document.querySelector('.prev-max-btn');
-const prevBtn = document.querySelector('.prev-btn');
-
 const nextMaxBtn = document.querySelector('.next-max-btn');
+
+const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 
 const numBtn = document.querySelector('.num-slider-btn');
 
 let numSliderBtn = 1;
 
-//счетчик
+//счетчик для +1280px
 nextBtn.addEventListener('click', () => {
     if (numSliderBtn < 6) {
         numSliderBtn++;
         numBtn.textContent = numSliderBtn.toString();
     }
-    randomShuffle()
+    randomShuffle();
+    disableBtn();
 });
 
 prevBtn.addEventListener('click', () => {
@@ -23,17 +24,20 @@ prevBtn.addEventListener('click', () => {
         numSliderBtn--;
         numBtn.textContent = numSliderBtn.toString();
     }
-    randomShuffle()
+    randomShuffle();
+    disableBtn();
 })
 
 prevMaxBtn.addEventListener('click', () => {
     numSliderBtn = 1;
     numBtn.textContent = numSliderBtn.toString();
+    disableBtn();
 });
 
 nextMaxBtn.addEventListener('click', () => {
     numSliderBtn = 6;
     numBtn.textContent = numSliderBtn.toString();
+    disableBtn();
 });
 
 //рандомные блоки pets-item по нажатию на кнопки
@@ -45,3 +49,23 @@ function randomShuffle() {
         }
     }
 }
+
+//добавление disable к пагинации
+function disableBtn() {
+    if (numSliderBtn === 1) {
+        prevBtn.classList.add('disable');
+        prevMaxBtn.classList.add('disable');
+    } else {
+        prevBtn.classList.remove('disable');
+        prevMaxBtn.classList.remove('disable');
+    }
+
+    if (numSliderBtn === 6) {
+        nextBtn.classList.add('disable');
+        nextMaxBtn.classList.add('disable');
+    } else {
+        nextBtn.classList.remove('disable');
+        nextMaxBtn.classList.remove('disable');
+    }
+}
+disableBtn();
